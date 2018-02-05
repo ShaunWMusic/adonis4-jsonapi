a0b283f35b8c732cbc9c56ff3a5c128c5f53e702
shaunwmusic <shaunwmusic@gmail.com>
2018-02-05 15:30:57 -0600

    update syntax and readme docs

--- providers/JsonApiProvider.js
+++ providers/JsonApiProvider.js
@@ -5,7 +5,7 @@ const { ServiceProvider } = require('@adonisjs/fold');
 class JsonApiProvider extends ServiceProvider {
 
   async register() {
-    this.app.bind('AdonisJsonApi/Middleware', (app) => {
+    this.app.bind('AdonisJsonApi/Middleware', (app) {
       const JsonApi = require('../src/JsonApiMiddleware');
 
       return new JsonApi(app.use);
--- src/JsonApiMiddleware.js
+++ src/JsonApiMiddleware.js
@@ -54,7 +54,7 @@ class JsonApi {
     });
   }
 
-  async handle({request, response, next}) => {
+  async handle({request, response, next}) {
     request.jsonApi = new JsonApiRequest(request);
 
     await next;
